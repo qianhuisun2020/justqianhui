44e2df7c5b277d13479548a9d0df60424546fa07
qianhuisun <qianhuisun2020@u.northwestern.edu>
2019-06-16 20:32:37 -0500

    Add contact info.

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -46,10 +46,18 @@ class Contact extends Component {
                     </div>
                     <div className="text">
                         <br/>
+                        > &nbsp; ./wechat
+                        <br/> 
+                        sun18516291197
+                        <br/>
+                    </div>
+                    <div className="text">
+                        <br/>
                         > &nbsp; ./professional 
                         <br/> 
                         <a href="https://github.com/qianhuisun" target="_blank">GitHub</a> &nbsp; 
                         <a href="https://www.linkedin.com/in/qianhui-sun-65369816a/" target="_blank">LinkedIn</a>
+                        <a href="https://www.facebook.com/Qianhui1996" target="_blank">Facebook</a>
                         <br/>
                     </div>
                     <div className="text">
